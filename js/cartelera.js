c29b0d20740dacbd55c2e1f1dd32d2e24aca7270
Ivan Pruyas <eipruyas@live.com.ar>
2022-12-28 21:07:45 -0300

    Agregado funciones JS

--- js/cartelera.js
+++ js/cartelera.js
@@ -10,9 +10,11 @@ const peliculas =
         calificacion: "+ 13",
         sinopsis: "Los Piratas de Sombrero de Paja van a un festival de música en la isla de Elegia a ver el concierto de Uta, una diva de enorme fama. Lo que iba a ser un concierto normal es interrumpido por una gran revelación por parte de Luffy: Uta es hija del Emperador Pirata Shanks.",
         estrellas: 4.7,
+        formato:'',
         cantidad: 0,
         fecha: '',
-        precio: 800
+        precio: 800,
+        imagen:'img/onepiecefilmred.jpg'
     },
 
     {
@@ -25,9 +27,11 @@ const peliculas =
         calificacion: "+ 16",
         sinopsis: "Cuatro años después de los sucesos de Halloween Kills: La Noche Aún No Termina, Laurie vive con su nieta Allyson (Andi Matichak) y está terminando de escribir su novela autobiográfica. Michael Myers no ha sido visto desde entonces. Laurie, después de permitir que el espectro de Michael determinara e impulsara su realidad durante décadas, ha decidido liberarse del miedo y la ira y aceptar la vida. Pero cuando un joven, Corey Cunningham (Rohan Campbell; The Hardy Boys, Un lugar para soñar), es acusado de matar a un niño que él cuidaba, se desencadena una serie de violencia y terror que obligará a Laurie a enfrentarse finalmente al mal que no puede controlar, de una vez por todas.",
         estrellas: 3.4,
+        formato:'',
         cantidad: 0,
         fecha: '',
-        precio: 700
+        precio: 700,
+        imagen:'img/halloweenlanochefinal.jpg'
     },
 
     {
@@ -40,9 +44,11 @@ const peliculas =
         calificacion: "+ 13",
         sinopsis: "La joven chatarrera Rey procedente de Jakku, Finn el ex stormtrooper que escapa de las garras la Primer Orden y Poe, el piloto de la Resistencia se unen a las leyendas galácticas en una épica aventura en la que se develarán misterios milenarios de la Fuerza e impactantes revelaciones del pasado.",
         estrellas: 4.0,
+        formato:'',
         cantidad: 0,
         fecha: '',
-        precio: 600
+        precio: 600,
+        imagen:'img/starwarsviii.jpg'
     },
 
     {
@@ -55,9 +61,11 @@ const peliculas =
         calificacion: "+ 13",
         sinopsis: "Un hombre que tiene su memoria dañada y no puede crear recuerdos a corto plazo, intentará rastrear al asesino de su esposa con la ayuda de una polaroid y tatuandose notas en su cuerpo.",
         estrellas: 5.0,
+        formato:'',
         cantidad: 0,
         fecha: '',
-        precio: 500
+        precio: 500,
+        imagen:'img/memento.jpg'
     },
 
     {
@@ -70,9 +78,11 @@ const peliculas =
         calificacion: "ATP",
         sinopsis: "Regresan los minions en una nueva aventura en la cual se encuentran con un joven Gru de tan solo 12 años cuyo único anhelo en la vida es convertirse en el villano más grande del mundo.",
         estrellas: 3.7,
+        formato:'',
         cantidad: 0,
         fecha: '',
-        precio: 300
+        precio: 300,
+        imagen:'img/minions2.jpg'
     },
 
     {
@@ -85,9 +95,11 @@ const peliculas =
         calificacion: "+ 13",
         sinopsis: "Un cajero de banco solitario (Ryan Reynolds) descubre que en realidad es un personaje de fondo en Free City, un videojuego de mundos virtuales donde todos los días pasa exactamente lo mismo. Hasta que un día decide convertirse en el personaje principal del juego y alterar las reglas.",
         estrellas: 4.5,
+        formato:'',
         cantidad: 0,
         fecha: '',
-        precio: 400
+        precio: 400,
+        imagen:'img/freeguy.jpg'
     }
 ]
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//Almacena la transaccion
+const transaccion = [];
+
 const contenedorbutaca = document.querySelector('.contenedor-sala');
 const butacas = document.querySelectorAll('.fila .butaca:not(.ocupado');
 const peliculaSeleccionada = document.getElementById('pelicula');
@@ -6,13 +9,17 @@ const fechaSeleccionada = document.getElementById('fecha');
 const horarioSeleccionado = document.getElementById('horario');
 const cantidad = document.getElementById('cantidad');
 const total = document.getElementById('total');
-
-verificarStorage();
+const botonCheckout = document.getElementById('checkout');
+const contenedorCheckout = document.querySelector('.contenedor-checkout');
+const alertPlaceholder = document.getElementById('alertaSeleccionButacas')
 
 let precioPelicula = parseInt(peliculaSeleccionada.value);
+let verificacionButacas = 0;
 let formatoPelicula = '';
 let fechaPelicula = '';
 let horarioPelicula = '';
+
+verificarStorage();
 
 //Actualiza cantidad y total
 function actualizarSeleccion() {
@@ -22,10 +29,14 @@ function actualizarSeleccion() {
 
     localStorage.setItem('indiceButacas', JSON.stringify(indiceButacas));
 
-    let cantidadButacasSeleccionadas = seleccionButacas.length;
-    
-    cantidad.innerText = cantidadButacasSeleccionadas;
-    total.innerText = cantidadButacasSeleccionadas * precioPelicula;
+    verificacionButacas = seleccionButacas.length;
+
+    if(verificacionButacas > 0) {
+        document.getElementById('alertaSeleccionButacas').innerHTML = '';
+    }     
+
+    cantidad.innerText = verificacionButacas;
+    total.innerText = verificacionButacas * precioPelicula;
 }
 
 function verificarStorage() {
@@ -49,20 +60,38 @@ function verificarStorage() {
 
     if (indiceFormatoSeleccionado !== null) {
         formatoSeleccionado.selectedIndex = indiceFormatoSeleccionado;
+        
+        formatoPelicula = localStorage.getItem('valorSeleccionFormato');
     }
 
     const indiceFechaSeleccionada = localStorage.getItem('indiceSeleccionFecha');
 
     if (indiceFechaSeleccionada !== null) {
         fechaSeleccionada.selectedIndex = indiceFechaSeleccionada;
+
+        fechaPelicula = localStorage.getItem('valorSeleccionFecha');
     }
 
     const indiceHorarioSeleccionado = localStorage.getItem('indiceSeleccionHorario');
 
     if (indiceHorarioSeleccionado !== null) {
         horarioSeleccionado.selectedIndex = indiceHorarioSeleccionado;
+
+        horarioPelicula = localStorage.getItem('valorSeleccionHorario');
     }
 }
+
+const alert = (message) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-warning alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    alertPlaceholder.append(wrapper)
+  }
 
 //Listeners
 peliculaSeleccionada.addEventListener('change', (e) => {
@@ -101,5 +130,54 @@ contenedorbutaca.addEventListener('click', (e) => {
     }
     actualizarSeleccion();
 });
+
+botonCheckout.addEventListener('click', (e) => {
+    if(verificacionButacas > 0) {
+        const pelicula = peliculas.find(pelicula => pelicula.id === peliculaSeleccionada.selectedIndex+1);
+
+        agregarTransaccion(pelicula, formatoPelicula, verificacionButacas, fechaPelicula, horarioPelicula);
+
+        mostarDatosCheckout();
+
+        contenedorCheckout.classList.toggle('checkout-active');
+    } else {
+        alert('Debe seleccionar alguna butaca')
+    }    
+});
+
+const mostarDatosCheckout = () => {
+    const contenedor = document.getElementById('contenedor-transaccion');
+    
+    transaccion.forEach(transaccion => {
+    contenedor.innerHTML = `
+    <div class="contenedor-informacion">
+    <div>
+        <p>Pelicula</p>
+        <p id="nombrePelicula">${transaccion.nombre}</p>
+        <p>Formato: <span id="formatoPelicula">${transaccion.formato}</span></p>
+        <p>Fecha: <span id="fechaHoraPelicula">${transaccion.fecha}</span></p>
+        <p>Cantidad Entradas: <span id="cantidadEntradas">${transaccion.cantidad}</span></p>
+        <p>Precio por Entrada: $<span id="precioEntrada">${transaccion.precio}</span></p>
+    </div>
+    <img src=${transaccion.imagen} width="200" alt="${transaccion.nombre}">
+    </div>
+    <p><small>Si esta correcto la transaccion, presione "Aceptar".</small></p>
+    <p><small>Si desea realizar alguna modificacion, presione en "Cancelar"</small></p>
+    <div class="contenedor-botones-checkout">
+        <div class="d-grid gap-2 d-md-block">
+            <button class="btn btn-primary" type="button" id="aceptarCheckout">Aceptar</button>
+            <button class="btn btn-secondary" type="button" id="cancelarCheckout">Cancelar</button>
+        </div>
+    </div>`
+});
+}
+
+const agregarTransaccion = (pelicula, formato, cantidad, fecha, horario) => {
+    pelicula.cantidad += cantidad;
+    pelicula.fecha = fecha + '. ' + horario;
+    pelicula.formato = formato;
+    
+    transaccion.push(pelicula)
+};
 
 actualizarSeleccion();
